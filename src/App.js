f30a1e36a3ab15604a28226b67d8a84a838c7073
Allan Stocco Rezende <allanstocco@hotmail.com>
2022-08-26 17:19:41 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Login from './pages/LoginPage'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
