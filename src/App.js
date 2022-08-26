a315b7ee359a43407622dfab41d96c9c65a5a8ce
Harrison Grant <Harry.grant99@googlemail.com>
2022-08-26 16:19:11 +0100

    added ability to grab login JWT token & createsuperuser on backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import Login from './pages/LoginPage'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AuthProvider>
+          <Login />
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
