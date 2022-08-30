31dbe53cda55ee9c92391a44e757062694ec8b2c
Harrison Grant <Harry.grant99@googlemail.com>
2022-08-30 20:04:06 +0100

    beginner modal ready

--- src/pages/Workout/workout.js
+++ src/pages/Workout/workout.js
@@ -24,6 +24,7 @@ export default function Workout() {
   let [stats, setStats] = useState(false)
   let [notes, setNotes] = useState([])
   let {authTokens, logoutUser} = useContext(AuthContext)
+  let [beginnerModal, setBeginnerModal] = useState(false)
 
     // const level = useSelector((state) => state.reducer.level);
     const dispatch = useDispatch();
@@ -68,8 +69,50 @@ export default function Workout() {
         logoutUser()
     }
 
+
+    function handleBeginner () {
+      setBeginnerModal(true)
+    }
+
     return (
     <>
+    {/* =======================Beginner Modal=======================*/}
+
+    {beginnerModal && (
+      <>
+            <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+            <div class="modal">
+              <div class="modal-box relative">
+                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <h3 class="text-lg font-bold">Our Beginner Program</h3>
+                <p class="py-4">If you're brand new to strength training <br></br>we recommend gaining foundational movements<br></br>
+                & the correct technique to prevent injury.</p>
+                <p class="py-4">Our beginner program has you train 3 days per <br></br>week; gaining foundational strength in classic<br></br> movement patterns. See for yourself!</p>
+                With this template, you alternate between <br></br>two workouts. e.g. ABA - week 1. BAB - week 2.
+                <br></br> 
+                <br></br>
+                
+                <p class="py-4">
+                  Workout A:<br></br>
+                  Bench Press: 3x8<br></br>
+                  Barbell Row: 3x8<br></br>
+                  Squat Variation: 3x8<br></br>
+                  Tricep Extensions: 3x10<br></br>
+                  Bicep Curls: 3x10<br></br>
+                </p>
+                <p class="py-4">
+                  Workout B:<br></br>
+                  Bench Press: 3x8<br></br>
+                  Barbell Row: 3x8<br></br>
+                  Leg Press: 3x8<br></br>
+                  Skull Crushers: 3x10<br></br>
+                  Hammer Curls: 3x10<br></br>
+                </p>
+                <br></br>
+                <button class="btn btn-primary">Select This Template</button>
+              </div>
+            </div>
+      </>)}
       {/* =======================Sidebar=======================*/}
     <NavbarDash />
     <div className='container-dash'>
@@ -129,7 +172,7 @@ export default function Workout() {
     <h2 class="card-title">Beginner Program</h2>
     <p></p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary">Select</button>
+    <label for="my-modal-3" class="btn modal-button btn-primary" onClick={handleBeginner}>Select</label>
     </div>
   </div>
 </div>
@@ -176,13 +219,7 @@ export default function Workout() {
 
 </div>
 </main>
-
-
         </motion.div>
-        
-
-
-
         </div>
       </>
         
